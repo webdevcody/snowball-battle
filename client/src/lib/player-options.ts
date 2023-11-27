51d7653321072ccad3b9b3843d7ef5e7e875e6ce
Heath Henley <heath.j.henley@gmail.com>
2023-11-27 18:42:18 -0500

    Move 'player options' into lib file

---
+++ client/src/lib/player-options.ts
@@ -0,0 +1,26 @@
+export const SANTA_COLORS = [
+  "Red",
+  "Green",
+  "Blue",
+  "Purple",
+  "Yellow",
+  "Teal",
+] as const;
+export type SantaOptions = typeof SANTA_COLORS;
+export type SantaColor = SantaOptions[number];
+
+type SantaIconDetails = {
+  image: string;
+  alt: string;
+  label: SantaColor;
+};
+export function getIconDetails(santaColor: SantaColor) {
+  return {
+    image: `santa-${santaColor.toLowerCase()}.png`,
+    alt: `A ${santaColor.toLowerCase()} santa icon`,
+    label: santaColor,
+  };
+}
+
+export const DEFAULT_SANTA_COLOR: SantaColor = "Red";
+export const DEFAULT_NICKNAME = "anonymous";
