4f8a29059ee238834f24ecf6a6c1a3938e373fc2
Heath Henley <heath.j.henley@gmail.com>
2023-11-29 17:57:57 -0500

    Add common input flags + config file changes to load

---
+++ common/input.ts
@@ -0,0 +1,6 @@
+export const UP: MoveDirection = 0x0001;
+export const DOWN: MoveDirection = 0x0010;
+export const LEFT: MoveDirection = 0x0100;
+export const RIGHT: MoveDirection = 0x1000;
+export const NONE: MoveDirection = 0x0000;
+export type MoveDirection = 0x0000 | 0x0001 | 0x0010 | 0x0100 | 0x1000;
