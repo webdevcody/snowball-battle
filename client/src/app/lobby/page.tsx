3aa34b932cca39c8e5693f63dba807f1fdee555c
Web Dev Cody <webdevcody@gmail.com>
2024-02-18 18:08:26 -0500

    hide header on loading screen

--- client/src/app/lobby/page.tsx
+++ client/src/app/lobby/page.tsx
@@ -47,74 +47,76 @@ export default function Lobby() {
 
   return (
     <main className="">
-      <Header />
       {lobbyState === "VIEW" && (
-        <section className="w-full text-white h-screen">
-          <div className="container mx-auto px-4 py-8">
-            <h1
-              className={`mt-8 text-4xl font-bold text-center mb-2 ${christmasFontNormal.className}`}
-            >
-              The Battle Lobby
-            </h1>
-            <h2 className={`text-xl text-center my-8`}>
-              <div className={christmasFontNormal.className}>{nickname}</div>
-              <Link
-                href="/"
-                className="ml-2 text-xs text-red-400 hover:text-red-500"
+        <>
+          <Header />
+          <section className="w-full text-white h-screen">
+            <div className="container mx-auto px-4 py-8">
+              <h1
+                className={`mt-8 text-4xl font-bold text-center mb-2 ${christmasFontNormal.className}`}
               >
-                Change Nickname
-              </Link>
-            </h2>
-
-            <div className="grid grid-cols-4 gap-12">
-              <div className="col-span-3">
-                {hasRooms && (
-                  <>
-                    <h2 className="text-2xl font-semibold mb-4 text-white">
-                      Current Games
-                    </h2>
-
-                    <div className="rounded-lg shadow-md">
-                      <div className="grid grid-cols-3 gap-4">
-                        {lobbies.map((lobby) => (
-                          <GameCard
-                            latency={getLatency(lobby.region)}
-                            key={lobby.roomId}
-                            lobby={lobby}
-                          />
-                        ))}
-                      </div>
-                    </div>
-                  </>
-                )}
-              </div>
-
-              <div
-                className={clsx({
-                  "col-span-2 col-start-2": !hasRooms,
-                  "col-span-1": !hasRooms,
-                })}
-              >
-                <h2
-                  className={`text-2xl font-semibold mb-4 text-white ${christmasFontNormal.className}`}
+                The Battle Lobby
+              </h1>
+              <h2 className={`text-xl text-center my-8`}>
+                <div className={christmasFontNormal.className}>{nickname}</div>
+                <Link
+                  href="/"
+                  className="ml-2 text-xs text-red-400 hover:text-red-500"
                 >
-                  Create a New Room
-                </h2>
+                  Change Nickname
+                </Link>
+              </h2>
+
+              <div className="grid grid-cols-4 gap-12">
+                <div className="col-span-3">
+                  {hasRooms && (
+                    <>
+                      <h2 className="text-2xl font-semibold mb-4 text-white">
+                        Current Games
+                      </h2>
+
+                      <div className="rounded-lg shadow-md">
+                        <div className="grid grid-cols-3 gap-4">
+                          {lobbies.map((lobby) => (
+                            <GameCard
+                              latency={getLatency(lobby.region)}
+                              key={lobby.roomId}
+                              lobby={lobby}
+                            />
+                          ))}
+                        </div>
+                      </div>
+                    </>
+                  )}
+                </div>
 
                 <div
-                  className="bg-gray-700 rounded-lg shadow-md p-4 flex flex-col
-                gap-8"
+                  className={clsx({
+                    "col-span-2 col-start-2": !hasRooms,
+                    "col-span-1": !hasRooms,
+                  })}
                 >
-                  <CreateRoomSection
-                    onRoomCreated={() => {
-                      setLobbyState("CREATING");
-                    }}
-                  />
+                  <h2
+                    className={`text-2xl font-semibold mb-4 text-white ${christmasFontNormal.className}`}
+                  >
+                    Create a New Room
+                  </h2>
+
+                  <div
+                    className="bg-gray-700 rounded-lg shadow-md p-4 flex flex-col
+                gap-8"
+                  >
+                    <CreateRoomSection
+                      onRoomCreated={() => {
+                        setLobbyState("CREATING");
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </section>
+          </section>
+        </>
       )}
 
       {lobbyState === "LOADING" && <LoadingLobbySpinner />}
